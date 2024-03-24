dee4308010bb473bafd50a0a7e362fd07899cbe7
Doan Quoc Hoa <109152800+quochoa304@users.noreply.github.com>
2024-03-24 20:15:06 +0700

    Update App.js

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -4,6 +4,9 @@ import HomePage from './Component/HomePage';
 import LoginPage from './Component/LoginPage';
 import RegisterPage from './Component/RegisterPage';
 import MemberPage from './Component/MemberPage';
+import CaloCalculator from './Component/CaloCalculator';
+import Classes from './Component/Classes';
+
 
 
 function App() {
@@ -14,8 +17,10 @@ function App() {
           <Route path="/" element={<HomePage />} exact />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/registration" element={<RegisterPage />} />
-          <Route path="/member" element={<MemberPage/>} />
+          <Route path="/member" element={<MemberPage />} />
           <Route path="*" element={<h1>Not Found</h1>} />
+          <Route path="/caloCalculator" element={<CaloCalculator />} />
+          <Route path="/classes" element={<Classes />} />
         </Routes>
       </div>
     </Router>
