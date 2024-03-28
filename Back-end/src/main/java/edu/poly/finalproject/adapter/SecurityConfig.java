f32ee1deb48863fe70b44ce049430c305e906430
Doan Quoc Hoa <109152800+quochoa304@users.noreply.github.com>
2024-03-29 00:46:17 +0700

    Login by google API

--- Back-end/src/main/java/edu/poly/finalproject/adapter/SecurityConfig.java
+++ Back-end/src/main/java/edu/poly/finalproject/adapter/SecurityConfig.java
@@ -1,6 +1,8 @@
 package edu.poly.finalproject.adapter;
 
+import edu.poly.finalproject.model.User;
 import edu.poly.finalproject.service.UserService;
+import edu.poly.finalproject.service.UserServiceImpl;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -9,12 +11,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -25,6 +30,9 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Map;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -32,10 +40,33 @@ import java.util.Arrays;
 public class SecurityConfig  {
 
     private final UserService userService;
+    private final UserServiceImpl userServiceImp;
+    @Bean
+    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService() {
+        DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
+        return request -> {
+            // Load the OAuth2 user using the default service
+            OAuth2User oAuth2User = delegate.loadUser(request);
+
+            // Extract user information from OAuth2User
+            Map<String, Object> attributes = oAuth2User.getAttributes();
+            String email = (String) attributes.get("email");
+
+            // Delegate to the custom method to save or update the user based on the extracted email
+            User user =userServiceImp.saveOrUpdateUserFromOAuth2(attributes);
+
+            // Here, you can create a new OAuth2User instance with your custom authorities if needed
+            // For simplicity, we just return the original oAuth2User
+            return oAuth2User;
+        };
+    }
+
+
 
     @Autowired
-    public SecurityConfig(UserService userService, BCryptPasswordEncoder passwordEncoder) {
+    public SecurityConfig(UserService userService, UserServiceImpl userServiceImp, BCryptPasswordEncoder passwordEncoder) {
         this.userService = userService;
+        this.userServiceImp = userServiceImp;
         this.passwordEncoder = passwordEncoder;
     }
 
@@ -64,27 +95,47 @@ public class SecurityConfig  {
     }
 
 
+
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .httpBasic(AbstractHttpConfigurer::disable)
-                .cors(Customizer.withDefaults())
+                .cors(withDefaults())
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeRequests((authz) -> authz
-                        .requestMatchers(
-                                "/",
-                                "/registration",
-                                "/buy-a-plan",
-                                "/login",
-                                "/calculateCalo",
-                                "/assets/**",
-                                "/js/**",
-                                "/css/**",
-                                "/img/**").permitAll()
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
-                        .anyRequest().authenticated()
+                .authorizeRequests((authz) -> {
+                            try {
+                                authz
+                                        .requestMatchers(
+                                                "/",
+                                                "/registration",
+                                                "/buy-a-plan",
+                                                "/user",
+                                                "/login",
+                                                "/calculateCalo",
+                                                "/assets/**",
+                                                "/js/**",
+                                                "/css/**",
+                                                "/img/**",
+                                                "/token/**").permitAll()
+                                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                                        .anyRequest().authenticated()
+                                        .and()
+                                        .oauth2Login(oauth2 -> oauth2
+                                                .loginPage("/login")
+                                                .defaultSuccessUrl("/member", true)
+                                                .failureUrl("/login?error=true")
+                                                .userInfoEndpoint(userInfo -> userInfo
+                                                        .userService(oAuth2UserService()) // Define this bean
+                                                )
+                                        );
+                            } catch (Exception e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
                 )
+                .oauth2Client(withDefaults())
                 .formLogin((formLogin) -> formLogin
                         .loginPage("/login")
                         .failureHandler(new AuthenticationFailureHandler() {
